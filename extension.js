dc7c2551e0b1e56060ae6eec52ec94d65c91a5d3
Alexander Kolenaty <akolenaty@protonmail.ch>
2025-06-17 08:55:22 +0200

    cleanup

--- extension.js
+++ extension.js
@@ -1,79 +1,28 @@
-/* extension.js
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * SPDX-License-Identifier: GPL-2.0-or-later
- */
 import GObject from 'gi://GObject';
 
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
 import {Extension, gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
-import {QuickToggle, SystemIndicator, QuickMenuToggle} from 'resource:///org/gnome/shell/ui/quickSettings.js';
-import {PopupMenuItem} from 'resource:///org/gnome/shell/ui/popupMenu.js';
-import * as WindowUtils from './window.js';
-import * as TilingUtils from './tiling.js';
+import {QuickToggle, SystemIndicator} from 'resource:///org/gnome/shell/ui/quickSettings.js';
+import * as DynamicUtils from './dynamic.js';
 import {BounceKeybindings} from './keybindings.js';
 
-const BounceToggle = GObject.registerClass(
-class BounceToggle extends QuickToggle {
+const DynamicTilingToggle = GObject.registerClass(
+class DynamicTilingToggle extends QuickToggle {
     constructor() {
         super({
-            title: _('Bounce'),
-            iconName: 'face-smile-symbolic',
-            toggleMode: true,
-        });
-
-        this.connect('notify::checked', () => {
-            if (this.checked) {
-                console.log('[Bounce] Toggle activated');
-                WindowUtils.enableForceCentering();
-            } else {
-                console.log('[Bounce] Toggle deactivated');
-                WindowUtils.disableForceCentering();
-            }
-        });
-    }
-});
-
-const TilingToggle = GObject.registerClass(
-class TilingToggle extends QuickMenuToggle {
-    constructor() {
-        super({
-            title: _('Tiling'),
+            title: _('Dynamic Tiling'),
             iconName: 'view-grid-symbolic',
             toggleMode: true,
-            menuEnabled: true,
         });
-
-        // Display the tiling mode (always Fibonacci)
-        const modeLabel = new PopupMenuItem(_(`Tiling Mode: ${TilingUtils.getModeString()}`));
-        modeLabel.sensitive = false;  // Make it non-clickable since there's only one mode
-
-        // Store a reference
-        this._modeMenuItem = modeLabel;
-        
-        // Add the menu item properly
-        this.menu.addMenuItem(modeLabel);
 
         this.connect('notify::checked', () => {
             if (this.checked) {
-                console.log('[Bounce] Tiling activated');
-                TilingUtils.enableTiling();
+                console.log('[Bounce] Dynamic tiling activated');
+                DynamicUtils.enableDynamicTiling();
             } else {
-                console.log('[Bounce] Tiling deactivated');
-                TilingUtils.disableTiling();
+                console.log('[Bounce] Dynamic tiling deactivated');
+                DynamicUtils.disableDynamicTiling();
             }
         });
     }
@@ -87,16 +36,12 @@ class BounceIndicator extends SystemIndicator {
         this._indicator = this._addIndicator();
         this._indicator.iconName = 'view-grid-symbolic';
 
-        // Add the bounce toggle
-        this._bounceToggle = new BounceToggle();
-        this.quickSettingsItems.push(this._bounceToggle);
-        
-        // Add the tiling toggle
-        this._tilingToggle = new TilingToggle();
-        this._tilingToggle.bind_property('checked',
+        // Add the dynamic tiling toggle
+        this._dynamicTilingToggle = new DynamicTilingToggle();
+        this._dynamicTilingToggle.bind_property('checked',
             this._indicator, 'visible',
             GObject.BindingFlags.SYNC_CREATE);
-        this.quickSettingsItems.push(this._tilingToggle);
+        this.quickSettingsItems.push(this._dynamicTilingToggle);
     }
 });
 
@@ -113,9 +58,8 @@ export default class BounceExtension extends Extension {
     }
 
     disable() {
-        // Make sure force centering and tiling are disabled
-        WindowUtils.disableForceCentering();
-        TilingUtils.disableTiling();
+        // Make sure dynamic tiling is disabled
+        DynamicUtils.disableDynamicTiling();
         
         // Clean up the indicator
         this._indicator.quickSettingsItems.forEach(item => item.destroy());
--- keybindings.js
+++ keybindings.js
@@ -1,28 +1,8 @@
-/* keybindings.js
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Gener    // Removed cycle tiling mode function as we only have Fibonacci mode nowse as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * SPDX-License-Identifier: GPL-2.0-or-later
- */
-
 import GObject from 'gi://GObject';
 import Meta from 'gi://Meta';
 import Shell from 'gi://Shell';
 
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
-import * as WindowUtils from './window.js';
-import * as TilingUtils from './tiling.js';
 import * as DynamicUtils from './dynamic.js';
 
 /**
@@ -33,34 +13,13 @@ class BounceKeybindings extends GObject.Object {
     constructor(extension) {
         super();
         this._extension = extension;
-        
-        // Define our keyboard shortcuts
-        this._buildBindingDefinitions();
     }
 
     /**
      * Enable all keybindings
      */
     enable() {
-        // Add the "b" keybinding to toggle bounce with Super+b
-        Main.wm.addKeybinding(
-            'toggle-bounce',
-            this._extension.getSettings(),
-            Meta.KeyBindingFlags.IGNORE_AUTOREPEAT,
-            Shell.ActionMode.NORMAL,
-            this._toggleBounce.bind(this)
-        );
-        
-        // Add the "z" keybinding to toggle static tiling with Super+z
-        Main.wm.addKeybinding(
-            'toggle-tiling',
-            this._extension.getSettings(),
-            Meta.KeyBindingFlags.IGNORE_AUTOREPEAT,
-            Shell.ActionMode.NORMAL,
-            this._toggleTiling.bind(this)
-        );
-        
-        // Add the "t" keybinding to toggle dynamic tiling with Super+t
+        // Add the "t" keybinding to toggle dynamic tiling with Ctrl+t
         Main.wm.addKeybinding(
             'toggle-dynamic-tiling',
             this._extension.getSettings(),
@@ -76,58 +35,10 @@ class BounceKeybindings extends GObject.Object {
      * Disable all keybindings
      */
     disable() {
-        // Remove the keybindings
-        Main.wm.removeKeybinding('toggle-bounce');
-        Main.wm.removeKeybinding('toggle-tiling');
+        // Remove the keybinding
         Main.wm.removeKeybinding('toggle-dynamic-tiling');
         
         console.log('[Bounce] Keybindings disabled');
-    }
-
-    /**
-     * Handler for toggling bounce with Super+b
-     */
-    _toggleBounce() {
-        console.log('[Bounce] Toggle triggered via keyboard shortcut');
-        
-        // Get the bounce toggle from the indicator
-        if (this._extension._indicator) {
-            const toggle = this._extension._indicator._bounceToggle;
-            
-            // Toggle the checked state
-            toggle.checked = !toggle.checked;
-            
-            // The toggle's notify::checked signal will handle calling centerAllWindows
-            // when checked becomes true, so we don't need to call it directly here
-            console.log(`[Bounce] Toggle state changed to: ${toggle.checked ? 'on' : 'off'}`);
-        } else {
-            // If the indicator isn't available for some reason, just center the windows directly
-            WindowUtils.centerAllWindows();
-        }
-    }
-    
-    /**
-     * Handler for toggling static tiling with Super+z
-     */
-    _toggleTiling() {
-        console.log('[Bounce] Static tiling toggle triggered via keyboard shortcut');
-        
-        // Get the tiling toggle from the indicator
-        if (this._extension._indicator) {
-            const toggle = this._extension._indicator._tilingToggle;
-            
-            // Toggle the checked state
-            toggle.checked = !toggle.checked;
-            
-            console.log(`[Bounce] Tiling state changed to: ${toggle.checked ? 'on' : 'off'}`);
-        } else {
-            // If the indicator isn't available for some reason, just toggle tiling directly
-            if (TilingUtils.isTilingEnabled()) {
-                TilingUtils.disableTiling();
-            } else {
-                TilingUtils.enableTiling();
-            }
-        }
     }
     
     /**
@@ -136,29 +47,24 @@ class BounceKeybindings extends GObject.Object {
     _toggleDynamicTiling() {
         console.log('[Bounce] Dynamic tiling toggle triggered via keyboard shortcut');
         
-        // Make sure we disable fixed tiling if it's active
-        if (TilingUtils.isTilingEnabled()) {
-            TilingUtils.disableTiling();
-        }
-        
-        // Toggle dynamic tiling
-        if (DynamicUtils.isDynamicTilingEnabled()) {
-            DynamicUtils.disableDynamicTiling();
-            console.log('[Bounce] Dynamic tiling disabled');
+        // Get the dynamic tiling toggle from the indicator and toggle it
+        if (this._extension._indicator) {
+            const toggle = this._extension._indicator._dynamicTilingToggle;
+            
+            // Toggle the checked state - this will trigger the notify::checked signal
+            // which will handle enabling/disabling dynamic tiling
+            toggle.checked = !toggle.checked;
+            
+            console.log(`[Bounce] Dynamic tiling state changed to: ${toggle.checked ? 'on' : 'off'}`);
         } else {
-            DynamicUtils.enableDynamicTiling();
-            console.log('[Bounce] Dynamic tiling enabled');
+            // Fallback: if the indicator isn't available, toggle directly
+            if (DynamicUtils.isDynamicTilingEnabled()) {
+                DynamicUtils.disableDynamicTiling();
+                console.log('[Bounce] Dynamic tiling disabled');
+            } else {
+                DynamicUtils.enableDynamicTiling();
+                console.log('[Bounce] Dynamic tiling enabled');
+            }
         }
-    }
-    
-    /**
-     * Build our binding definitions
-     */
-
-    /**
-     * Build our binding definitions
-     */
-    _buildBindingDefinitions() {
-        // We could add more keybindings here in the future
     }
 });
